e26f9a153cc507e3dadaf8411b45787a2b9a4bc9
Evan Rutten <contact@evanrutten.com>
2023-09-21 14:21:26 -0400

    Rename folders (again)

--- taskmate_project/asgi.py
+++ taskmate_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for todo_site project.
+ASGI config for taskmate_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taskmate_project.settings')
 
 application = get_asgi_application()
